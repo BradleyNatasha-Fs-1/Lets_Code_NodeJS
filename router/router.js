bd7f6f8de564cc4ab5b09d80da3adedb477df25b
BradleyNatasha-FS-1 <92743076+BradleyNatasha-Fs-1@users.noreply.github.com>
2023-09-06 21:12:46 -0500

    1.5 update

--- router/router.js
+++ router/router.js
@@ -1,7 +1,7 @@
 const express =require("express");
 const router = express.Router();
 
-// http://Localhost:3000/exercise
+//http://Localhost:3000/exercise
 router.get("/", (req,res, next)=>{
     res.status(200).json({
         message: "Successful - GET",
@@ -22,6 +22,42 @@ router.get("/:id", (req,res,next)=>{
             hostname: req.hostname,
             method: req.method,
         },
+    });
+});
+
+//http://Localhost:3000/exercise/89
+router.put("/:id", (req,res,next)=>{
+    const id = req.params.id;
+    res.status(200).json({
+        message: "Successful - PUT by ID",
+        id: id,
+        metadata:{
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+});
+
+//http://Localhost:3000/exercise
+router.post("/", (req,res,next)=>{
+    res.status(200).json({
+        message: "Successful - POST",
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+});
+
+//http://Localhost:3000/exercise/9
+router.delete("/:id", (req,res,next)=>{
+    //const id = req.params.id;
+    res.status(200).json({
+        message: "Successful - DELETE by ID",
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        }
     });
 });
 
